8c83d72d401245430c75bdaceb6f7953a2745941
tinsutesfu <yetesfayes@gmail.com>
2024-07-09 10:53:19 -0700

    modify image uploads

--- routes/itroute.js
+++ routes/itroute.js
@@ -2,16 +2,24 @@ import express from 'express';
 import { addit, addrating, itlist, removeit } from '../controllers/itcontroller.js';
 import multer from 'multer';
 import verifyJWT from '../middleware/verifyjwt.js';
-
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 const itrouter=express.Router();
 
-const storage =multer.diskStorage({
-    destination:'uploads',
-    filename:(req,File,cb)=>{
-        return cb(null,`${Date.now()}${File.originalname}`)
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, '../../amazon/public/uploads'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}${file.originalname}`);
     }
-});
+  });
+  
 
 const upload=multer({storage:storage});
 itrouter.post('/add',upload.single('image'),addit);
